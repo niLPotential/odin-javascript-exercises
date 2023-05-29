58e166184cd23524653ab62a3a1ed96c4fe5b876
niLPotential <niLPotential@proton.me>
2023-05-29 16:42:24 +0900

    Solve 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (str, n) {
+    if (n < 0) {
+        return "ERROR"
+    }
 
+    let result = ""
+
+    for (i = 0; i < n; i++) {
+        result += str
+    }
+
+    return result
 };
 
 // Do not edit below this line
