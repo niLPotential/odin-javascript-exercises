982690ff4561f432211de083375f55b31805c43a
niLPotential <niLPotential@proton.me>
2023-05-29 17:16:30 +0900

    Solve sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+    if (!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0) {
+        return "ERROR"
+    }
 
+    let sum = 0;
+    const start = a < b ? a : b;
+    const end = a > b ? a : b;
+    // for (let i = start; i <= end; i++) {
+    //     sum += i;
+    // }
+    // return sum;
+    return (end + start) * (end - start + 1) / 2
 };
 
 // Do not edit below this line
